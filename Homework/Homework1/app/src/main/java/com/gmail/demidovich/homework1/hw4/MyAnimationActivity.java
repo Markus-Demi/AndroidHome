8945f05c608064d5d6e64c687723343ba2658d08
Markus-Demi <m4rk.demi@gmail.com>
2018-02-19 19:35:13 +0300

    Homework4. Added animation to Owl (owl get high a little bit).

--- Homework/Homework1/app/src/main/java/com/gmail/demidovich/homework1/hw4/MyAnimationActivity.java
+++ Homework/Homework1/app/src/main/java/com/gmail/demidovich/homework1/hw4/MyAnimationActivity.java
@@ -1,8 +1,10 @@
 package com.gmail.demidovich.homework1.hw4;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gmail.demidovich.homework1.R;
@@ -16,5 +18,10 @@ public class MyAnimationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_myanimation);
 
         mTextView = findViewById(R.id.text_anim);
+
+        ImageView imageView = findViewById(R.id.owl_anim_id);
+        AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getDrawable();
+
+        animationDrawable.start();
     }
 }
